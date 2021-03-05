6b5e06d81d0df5dbf36ff60af5e866acfb76a1be
TaskerSolutions <79877715+TaskerSolutions@users.noreply.github.com>
2021-03-05 21:56:53 +1300

    Update level.js

--- script/level.js
+++ script/level.js
@@ -12,7 +12,7 @@ function startNextLevel(level) {
 	// retrieve best times
 	retrieveBestTimes();
 	// print current best time to html doc
-	$('#best-time').html(bestTimes[currentLevel]);
+	if (bestTimes[currentLevel] != null) {$('#best-time').html(bestTimes[currentLevel]);}
 
 	// initializes components of each level
 	createLevel(currentLevel);
